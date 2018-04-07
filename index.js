7fd03397ef3c6a40a37ccf05643e4a48356f1f86
Wei Xia <xw900812@gmail.com>
2018-04-07 12:11:44 -0400

    Add day and 52 week range

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ alfy.fetch(`https://api.iextrading.com/1.0/stock/${alfy.input}/batch?types=quote
     .then(result => {
 
         const items  = [{
-			"title": result.quote.symbol + ": " + result.quote.close + ", change: " + result.quote.changePercent*100+"%",
-			"subtitle": "Today high: " + result.quote.high +", Today Low: " + result.quote.low,
+			"title": result.quote.symbol + ": " + result.quote.close.toFixed(2) + ", change: " + (result.quote.changePercent*100).toFixed(2)+"%",
+			"subtitle": "Day Low/High: " + result.quote.low.toFixed(2) +" - " + result.quote.high.toFixed(2) + " | 52 Week Low/High: " + result.quote.week52Low.toFixed(2) +" - " + result.quote.week52High.toFixed(2),
 			}];
 		alfy.output(items);
     });
