c4e2c9b568b221235a5ac6314fd6b5a186b3933e
Wei Xia <xw900812@gmail.com>
2018-04-10 22:28:00 -0400

    update API

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ alfy.fetch(`https://api.iextrading.com/1.0/stock/${alfy.input}/batch?types=quote
     .then(result => {
 
         const items  = [{
-			"title": result.quote.symbol + ": " + result.quote.close.toFixed(2) + ", change: " + (result.quote.changePercent*100).toFixed(2)+"%",
+			"title": result.quote.symbol + ": " + result.quote.iexRealtimePrice.toFixed(2) + ", change: " + (result.quote.changePercent*100).toFixed(2)+"%",
 			"subtitle": "Day Low/High: " + result.quote.low.toFixed(2) +" - " + result.quote.high.toFixed(2) + " | 52 Week Low/High: " + result.quote.week52Low.toFixed(2) +" - " + result.quote.week52High.toFixed(2),
 			}];
 		alfy.output(items);
