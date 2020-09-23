a752e5e38f1698767487135a29b12a384043b85d
Wei Xia <xw900812@gmail.com>
2020-09-22 23:35:11 -0400

    feat: display price change in subject and error handling

--- index.js
+++ index.js
@@ -2,15 +2,44 @@ const alfy = require("alfy");
 
 const apiToken = process.env.token;
 
-alfy.fetch(`https://cloud.iexapis.com/v1/stock/${alfy.input}/quote?token=${apiToken.trim()}`)
-	.then(result => {
-		// console.log(result);
-		const percentage = result.changePercent * 100;
-		const items = [{
-			"title": result.symbol + ": " + result.latestPrice,
-			"subtitle": "Change: " + percentage.toFixed(2) + "% | 52 Week Low/High: " + result.week52Low.toFixed(2) + " - " + result.week52High.toFixed(2),
-		}];
-		alfy.output(items);
-	});
+alfy
+  .fetch(
+    `https://cloud.iexapis.com/v1/stock/${
+      alfy.input
+    }/quote?token=${apiToken.trim()}`
+  )
+  .then((result) => {
+    const percentage = (result.changePercent * 100).toFixed(2);
+    const week52Low = result.week52Low.toFixed(2);
+    const week52High = result.week52High.toFixed(2);
+    const extendedChange = (result.extendedChangePercent * 100).toFixed(2);
+    let emoji = "📈";
 
-		// ", change: " + (result.changePercent*100).toFixed(2)+"%"
+    if (percentage < 0) {
+      emoji = "📉";
+    }
+
+    const items = [
+      {
+        title:
+          result.symbol +
+          ": " +
+          result.latestPrice +
+          " | Change: " +
+          percentage +
+          "%" +
+          emoji,
+        subtitle:
+          "Extended Change Percent:" +
+          extendedChange +
+          "% | 52 Week Low/High: " +
+          week52Low +
+          " - " +
+          week52High,
+      },
+    ];
+    alfy.output(items);
+  })
+  .catch((err) => {
+    alfy.error("Stock Is " + err.statusMessage);
+  });
